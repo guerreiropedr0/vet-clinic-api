76cfbfaa53940e77272058ebac6e6e087a279f87
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-10-28 18:10:08 +0100

    Add validation for Vet model

--- app/models/vet.rb
+++ app/models/vet.rb
@@ -1,2 +1,8 @@
 class Vet < ApplicationRecord
+  validates :name, presence: true, length: { in: 2..200 }
+  validates :age, presence: true, numericality: {
+    greater_than_or_equal_to: 18,
+    less_than_or_equal_to: 80
+  }
+  validates :graduation_date, presence: true
 end
