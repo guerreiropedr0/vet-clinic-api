c73e12b397b4ae0173cf1bd08f0f0f9e414053ac
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-10-28 18:52:59 +0100

    Add model validation for Animal model

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,3 +1,11 @@
 class Animal < ApplicationRecord
   belongs_to :owner
+
+  validates :name, presence: true, length: { in: 2..100 }
+  validates :birth_date, presence: true
+  validates :neutered, presence: true, inclusion: [true, false]
+  validates :weight_kg, presence: true, numericality: {
+    greater_than_or_equal_to: 0.05,
+    less_than_or_equal_to: 200
+  }
 end
