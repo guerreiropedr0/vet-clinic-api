201e038c4695107691572b797ff37957d79b551d
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-10-28 18:05:44 +0100

    Add model validation specs for Vet model

--- spec/models/vet_spec.rb
+++ spec/models/vet_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Vet, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    context 'name attribute' do
+      it { should validate_presence_of(:name) }
+      it { should validate_length_of(:name).is_at_least(2).is_at_most(200) }
+    end
+
+    context 'age attribute' do
+      it { should validate_presence_of(:age) }
+      it { should validate_numericality_of(:age).is_greater_than_or_equal_to(18).is_less_than_or_equal_to(80) }
+    end
+
+    context 'graduation_date attribute' do
+      it { should validate_presence_of(:graduation_date) }
+    end
+  end
 end
