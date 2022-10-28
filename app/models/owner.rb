ef52b81381ba9c50fb20136f8b79cb4b34d85d3a
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-10-28 17:45:18 +0100

    Add model validation and model specs for Owner

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,2 +1,9 @@
 class Owner < ApplicationRecord
+  validates :name, presence: true, length: { in: 2..200 }
+  validates :age, numericality: {
+    greater_than_or_equal_to: 16,
+    less_than_or_equal_to: 140,
+    allow_nil: true
+  }
+  validates :email, uniqueness: true
 end
--- spec/models/owner_spec.rb
+++ spec/models/owner_spec.rb
@@ -1,4 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Owner, type: :model do
+  describe 'validations' do
+    context 'name attribute' do
+      it { should validate_presence_of(:name) }
+      it { should validate_length_of(:name).is_at_least(2).is_at_most(200) }
+    end
+
+    context 'age attribute' do
+      it { should validate_numericality_of(:age).is_greater_than_or_equal_to(16).is_less_than_or_equal_to(140) }
+    end
+
+    context 'email attribute' do
+      subject { FactoryBot.create(:owner) }
+      it { should validate_uniqueness_of(:email) }
+    end
+  end
 end
