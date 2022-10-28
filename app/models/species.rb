b7ddc9a489522f8ca96942c49c2182e1d1d47302
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-10-28 19:49:59 +0100

    Add model validation for Species model

--- app/models/species.rb
+++ app/models/species.rb
@@ -1,3 +1,5 @@
 class Species < ApplicationRecord
   has_many :animals
+
+  validates :name, presence: true, uniqueness: true, length: { in: 2..100 }
 end
--- spec/models/species_spec.rb
+++ spec/models/species_spec.rb
@@ -7,6 +7,8 @@ RSpec.describe Species, type: :model do
 
   describe 'validations' do
     context 'name attribute' do
+      subject { FactoryBot.create(:species) }
+      it { should validate_presence_of(:name) }
       it { should validate_uniqueness_of(:name) }
       it { should validate_length_of(:name).is_at_least(2).is_at_most(100) }
     end
