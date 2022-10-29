16ca44b959737a6b271dc09494460045e71a2a5a
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-10-29 13:21:19 +0100

    Add validation to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
          jwt_revocation_strategy: JwtDenylist
 
   enum role: %i[user mod admin] # animal owner, vet, admin
+
+  validates :name, presence: true, length: { in: 2..200 }
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true, length: { in: 5..100 }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,14 +8,16 @@ RSpec.describe User, type: :model do
     end
 
     context 'email attribute' do
+      subject { FactoryBot.create(:user) }
       it { should validate_presence_of(:email) }
-      it { should validate_uniqueness_of(:email) }
+      it { should validate_uniqueness_of(:email).case_insensitive }
       it { should allow_value('johndoe@domain.com').for(:email) }
       it { should_not allow_value('a_random_string').for(:email) }
     end
 
     context 'password attribute' do
       it { should validate_presence_of(:password) }
+      it { should validate_length_of(:password).is_at_least(5).is_at_most(100) }
     end
   end
 end
