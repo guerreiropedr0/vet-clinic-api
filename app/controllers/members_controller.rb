24776eaaad9c300393e11cec7da1bd4a8d2f037e
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-10-29 12:00:25 +0100

    Fix linters and requests getting an html response

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -10,8 +10,10 @@ class MembersController < ApplicationController
   end
 
   private
+
   def get_user_from_token
-    jwt_payload = JWT.decode(request.headers['Authorization'].split(' ')[1], Rails.application.credentials.devise[:jwt_secret_key]).first
+    jwt_payload = JWT.decode(request.headers['Authorization'].split(' ')[1],
+                             Rails.application.credentials.devise[:jwt_secret_key]).first
 
     user_id = jwt_payload['sub']
     user = User.find(user_id.to_s)
