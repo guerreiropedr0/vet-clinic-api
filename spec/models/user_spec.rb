eda9eefdeb7364db7f74ba759bce7bc27c816d4c
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-10-29 13:10:24 +0100

    Add validation specs for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    context 'name attribute' do
+      it { should validate_presence_of(:name) }
+      it { should validate_length_of(:name).is_at_least(2).is_at_most(200) }
+    end
+
+    context 'email attribute' do
+      it { should validate_presence_of(:email) }
+      it { should validate_uniqueness_of(:email) }
+      it { should allow_value('johndoe@domain.com').for(:email) }
+      it { should_not allow_value('a_random_string').for(:email) }
+    end
+
+    context 'password attribute' do
+      it { should validate_presence_of(:password) }
+    end
+  end
 end
