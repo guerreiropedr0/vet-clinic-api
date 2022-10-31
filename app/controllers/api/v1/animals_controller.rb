7f24aff38a668dc63667e8e615e9c210ea639602
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-10-31 12:57:07 +0000

    Creating an animal shows species relationship with serializer

--- app/controllers/api/v1/animals_controller.rb
+++ app/controllers/api/v1/animals_controller.rb
@@ -17,8 +17,9 @@ class Api::V1::AnimalsController < ApplicationController
   def create
     @animal = Animal.new(animal_params)
 
+
     if @animal.save
-      render json: @animal, status: :created
+      render json: AnimalSerializer.new(@animal), status: :created
     else
       render json: @animal.errors, status: :unprocessable_entity
     end
